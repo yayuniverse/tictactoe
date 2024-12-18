fc8249ca8608289adeadfe535b52da6c1df2c9d6
yay <yaytrxsh@proton.me>
2024-12-18 20:44:02 +0100

    fix input validator and add comments

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function gameBoard() {
         turnCount++
 
         // check that input is valid
-        if (i < 0 || i > 9) return
+        if (i < 1 || i > 9) return
 
-        // add appropiate turn-based input
+        // add appropriate turn-based input
         if (turnCount % 2 !== 0) {
             board[i-1] = "X"
         } else if (turnCount % 2 === 0) {
